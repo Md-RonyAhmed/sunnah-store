86de3fc1edf28747dddc98973ced444250b1be5e
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2024-12-11 12:05:36 +0600

    [ADD] Modal added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import Ebook from "../../components/Ebook";
 import Banner from "../../components/Shared/Header/Banner/Banner";
+import AdvertisementModal from "../../components/AdvertisementModal";
 
 const Home = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    // Check the localStorage value and update the state
+    const checkModalState = () => {
+      const isModalShown = localStorage.getItem("modal");
+      if (isModalShown === "false" || !isModalShown) {
+        setShowModal(true);
+        localStorage.setItem("modal", "true");
+      }
+    };
+
+    // Initial check on component mount
+    checkModalState();
+    // Set a timer to reset the localStorage value to "false" after 24 hours
+    const resetTimer = setTimeout(() => {
+      localStorage.setItem("modal", "false");
+      checkModalState();
+    }, 50000); // 24 hours in milliseconds
+
+    // Cleanup the timer
+    return () => clearTimeout(resetTimer);
+  }, [showModal]);
+
   return (
     <>
       <div>
@@ -9,6 +35,7 @@ const Home = () => {
       </div>
       <h1>Home</h1>
       <Ebook />
+      <AdvertisementModal showModal={showModal} setShowModal={setShowModal} />
     </>
   );
 };
