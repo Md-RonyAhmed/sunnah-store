cd6c6ba18d84f851c127e4c5ec81ab2dd98eb876
khanRabiul <krabiulislam2@gmail.com>
2024-12-13 19:23:49 +0600

    tailwind.config is modified to implement marqee animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 import withMT from "@material-tailwind/react/utils/withMT";
- 
+
 export default withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      container:{
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marqueeBack: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marqueeBack: "marqueeBack 25s linear infinite",
+      },
+      container: {
         center: true,
-        padding:"1.25rem"
-      }
+        padding: "1.25rem",
+      },
     },
   },
   plugins: [],
 });
-
