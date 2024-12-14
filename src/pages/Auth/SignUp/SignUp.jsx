1f941f0ba6a722350af8adf326a9ad6daad8518e
mdhasibulhasan13 <hasibulhasan7770@gmail.com>
2024-12-14 22:51:16 +0600

    [UPDATE] sign_up page updated

--- src/pages/Auth/SignUp/SignUp.jsx
+++ src/pages/Auth/SignUp/SignUp.jsx
@@ -1,7 +1,87 @@
 
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
+
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <Card color="transparent" className="mt-32 items-center" shadow={false}>
+      <Typography variant="h4" className="text-[#00BF63]">
+        Sign Up
+      </Typography>
+      <Typography color="gray" className="mt-2 font-normal w-80 max-w-screen-lg sm:w-auto">
+        Welcome to Sunna Store! Enter your details to register.
+      </Typography>
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <div className="mb-1 flex flex-col gap-4">
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Your Name
+          </Typography>
+          <Input
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-[#00BF63]"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Your Email
+          </Typography>
+          <Input
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-[#00BF63]"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Password
+          </Typography>
+          <Input
+            type="password"
+            size="lg"
+            placeholder="********"
+            className=" !border-t-blue-gray-200 focus:!border-[#00BF63]"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+        </div>
+        <Checkbox className="checked:bg-[#00BF63] checked:border-[#00BF63] border-[#00BF63]"
+          label={
+            <Typography
+              variant="small"
+              color="gray"
+              className="flex items-center font-normal"
+            >
+              I agree the
+              <a
+                href="#"
+                className="font-medium transition-colors hover:text-[#00BF63]"
+              >
+                &nbsp;Terms and Conditions
+              </a>
+            </Typography>
+          }
+          containerProps={{ className: "-ml-2.5" }}
+        />
+        <Button className="mt-6 bg-[#00BF63]" fullWidth>
+          sign up
+        </Button>
+        <Typography color="gray" className="mt-4 text-center font-normal">
+          Already have an account?{" "}
+          <a href="#" className="font-medium text-[#00BF63]">
+            Sign In
+          </a>
+        </Typography>
+      </form>
+    </Card>
   )
 }
 
