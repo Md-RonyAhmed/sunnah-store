f49ac63059f21b2a416ea05c8d7fda43b3a13e4d
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2024-12-14 15:54:28 +0600

    [DONE] Products Updated

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,16 +1,23 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import ProductCard from "./ProductCard";
 
 const Products = () => {
   const {
     data: { data: products },
   } = useLoaderData();
-  
+
+  const { key } = useParams();
+
   return (
-    <div className="my-32 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 ">
-      {products.map((product) => (
-        <ProductCard key={product._id} product={product} />
-      ))}
+    <div className="mt-28">
+      <h1 className="text-2xl border-b-4 border-[#00BF63] w-fit capitalize">
+        {key ? key : "all products"}
+      </h1>
+      <div className="mb-32 mt-6 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-4">
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
