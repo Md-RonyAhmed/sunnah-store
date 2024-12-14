6476a64b8a7571662258384348ed5a3104dc715a
sabbirmms <bishponda@gmail.com>
2024-12-14 22:27:56 +0600

    [ADD] Added FilterSection Component

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,16 +1,22 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import ProductCard from "./ProductCard";
 import ProductsPagination from "./ProductsPagination";
+import { useState } from "react";
+import FilterSection from "./Filtering/FilterSection";
 
 const Products = () => {
   const {
     data: { data: products },
   } = useLoaderData();
 
+  const [sortBy, setSortBy] = useState(0);
+
   const { key } = useParams();
 
   return (
     <div className="mt-28">
+      <FilterSection setSortBy={setSortBy} />
+
       <h1 className="text-2xl border-b-4 border-[#00BF63] w-fit capitalize">
         {key
           ? key === "groceries"
