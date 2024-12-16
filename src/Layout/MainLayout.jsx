c4439f68824343e4df75b2b01c4c99ed7a87cfa7
SabbirMMS <bishponda@gmail.com>
2024-12-16 22:05:15 +0600

    [Added] Auto Scroll to top when home outlet component/route changes

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,4 +1,5 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "../components/Shared/Header/Header";
 import Footer from "../components/Shared/Footer/Footer";
 import Loading from "../components/Shared/Loading";
@@ -6,6 +7,13 @@ import ScrollToTopBtn from "../components/Shared/ScroollToTop/ScrollToTopBtn";
 
 const MainLayout = () => {
   const navigation = useNavigation();
+  
+  const location = useLocation();
+  // Scroll to top when the location changes
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [location]);
+
   return (
     <div>
       <ScrollToTopBtn />
