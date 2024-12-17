27692854377ba5b47987409760da959f3e96b414
SabbirMMS <bishponda@gmail.com>
2024-12-17 16:44:23 +0600

    [Added] Added Categories option at product page

--- src/pages/Products/Filtering/FilterSection.jsx
+++ src/pages/Products/Filtering/FilterSection.jsx
@@ -1,4 +1,9 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { categoryData } from "../../Home/categories/Categories";
+
 const FilterSection = ({ setSortBy }) => {
+  const navigate = useNavigate();
+  const { key } = useParams();
   // Options for sorting
   const sortOptions = [
     { id: 0, label: "Select Sorting Option" },
@@ -8,24 +13,51 @@ const FilterSection = ({ setSortBy }) => {
     { id: 4, label: "Top Sales" },
   ];
 
-  // Handle selection change
-  const handleChange = (e) => {
+  // Handle sorting selection change
+  const handleSortChange = (e) => {
     const selectedValue = parseInt(e.target.value); // Get selected integer value
     setSortBy(selectedValue); // Trigger the sorting function
   };
 
+  // Handle category selection change
+  const handleCategoryChange = (e) => {
+    const selectedPath = e.target.value;
+    if (selectedPath) {
+      navigate(`/products${selectedPath}`); // Redirect to the selected category
+    }
+  };
+
   return (
-    <div className="flex flex-col lg:flex-row justify-between items-center pb-2 mb-2 bg-white rounded-lg">
-      <h3 className="text-xl ">Filter Items</h3>
+    <div className="flex flex-col lg:flex-row justify-start gap-5 items-center pb-4 mb-4 bg-white rounded-lg space-y-3 lg:space-y-0">
+      <h3 className="text-xl">Filter Items</h3>
+
+      {/* Sorting Dropdown */}
       <div className="relative">
         <select
           id="sortBy"
           className="block w-full px-4 py-2 text-sm border rounded-lg shadow-sm bg-white border-gray-300 text-gray-700 focus:outline-none"
-          onChange={handleChange}
+          onChange={handleSortChange}
         >
           {sortOptions.map((option) => (
             <option key={option.id} value={option.id}>
               {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* Category Dropdown */}
+      <div className="relative">
+        <select
+          id="categories"
+          className="block w-full px-4 py-2 text-sm border rounded-lg shadow-sm bg-white border-gray-300 text-gray-700 focus:outline-none"
+          onChange={handleCategoryChange}
+          value={`/${key}`}
+        >
+          <option value="">Select Category</option>
+          {categoryData.map((category) => (
+            <option key={category.path} value={category.path}>
+              {category.catName}
             </option>
           ))}
         </select>
