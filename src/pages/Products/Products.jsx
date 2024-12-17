7d6bec6146567b8624a561eb3a0d4cee017fcc99
SabbirMMS <bishponda@gmail.com>
2024-12-17 10:45:31 +0600

    [Updated] Removed Re-Loading on products

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -57,7 +57,7 @@ const Products = () => {
     setSortedProducts(sorted);
   }, [sortBy, products, inStock]);
 
-  if (isLoading || (sortedProducts.length === 0 && products.length > 0)) {
+  if (isLoading/* || (sortedProducts.length === 0 && products.length > 0)*/) {
     // handle waiting time with a loading screen when program is running to set data properly!
     return <Loading />;
   }
