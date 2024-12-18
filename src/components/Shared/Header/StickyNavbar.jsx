c9085e18ea5e85d6be985e7ce908d88ce42aa011
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2024-12-18 18:39:38 +0600

    Updated mequee

--- src/components/Shared/Header/StickyNavbar.jsx
+++ src/components/Shared/Header/StickyNavbar.jsx
@@ -12,10 +12,8 @@ import logo from "../../../assets/images/logo.png";
 import { FiShoppingCart } from "react-icons/fi";
 import { FaHeart } from "react-icons/fa";
 
-import Marquee from "./Marqee";
-
 import { AuthContext } from "../../../contexts/AuthContext";
-
+import Marquee from "./Marquee";
 
 export function StickyNavbar() {
   const [openNav, setOpenNav] = useState(false);
@@ -152,13 +150,10 @@ export function StickyNavbar() {
 
   return (
     <div className="w-full fixed top-0 z-40 bg-[#FBFFFF] shadow-sm">
-
       <Marquee />
-     
 
       <Navbar className="px-0 py-1 mx-auto rounded-none shadow-none">
         <div className="flex flex-wrap items-center justify-between text-blue-gray-900">
-
           {/* Logo & Search Bar */}
           <div className="order-1 font-medium cursor-pointer">
             <div className="flex flex-col items-start justify-start gap-6 md:flex-row md:items-center md:justify-center">
