063342d95fa88cb57b20b26c125cb9d6d2b808c7
SabbirMMS <bishponda@gmail.com>
2024-12-25 01:19:03 +0600

    sample

--- src/pages/Dashboard/Products/Forms/BaseForm.jsx
+++ src/pages/Dashboard/Products/Forms/BaseForm.jsx
@@ -1,5 +1,18 @@
 function BaseForm() {
-  return <div>Form Here</div>;
+  return (
+    <div>
+      <pre>
+        &quot;image&quot;:
+        &quot;https://jakevdp.github.io/PythonDataScienceHandbook/figures/PDSH-cover.png&quot;,
+        &quot;name&quot;: &quot;Python for Data Science&quot;,
+        
+        &quot;&quot;, &quot;status&quot;: true, &quot;price&quot;: 3200,
+        &quot;description&quot;: &quot;A beginner&apos;s guide to Python in data
+        science.&quot;, &quot;keyFeatures&quot;: &quot;author&quot;: &quot;Jane
+        Smith&quot; &quot;pages&quot;: 350
+      </pre>
+    </div>
+  );
 }
 
 export default BaseForm;
