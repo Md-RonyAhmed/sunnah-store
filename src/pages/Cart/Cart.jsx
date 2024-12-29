b693e39bd698337e4180c2abb220e6efaab93ad1
faruq-dev <faruq2109@gmail.com>
2024-12-29 20:53:08 +0600

    Add responsive design and styling to Cart page components

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -19,11 +19,11 @@ const Cart = () => {
 
   return (
     <>
-      <div className="mt-44 mb-6 w-fit">
+      <div className="mt-56 md:mt-44 mb-6 w-fit">
         <GoBack />
       </div>
       
-      <div className="mb-6 p-6 bg-white shadow-md rounded-lg">
+      <div className="mb-6 p-0 md:p-6 bg-white md:shadow-md rounded-lg">
         <Helmet>
           <title>Sunnah Store | Cart</title>
         </Helmet>
@@ -47,6 +47,7 @@ const Cart = () => {
           </div>
         ) : (
           <div>
+            {/* Clear cart */}
             <button
               className="flex items-center gap-2 mb-3 text-red-400 hover:underline hover:underline-offset-2 cursor-pointer hover:text-red-300 w-fit"
               onClick={removeAllFromCart}
@@ -56,12 +57,14 @@ const Cart = () => {
                 <MdRemoveShoppingCart color="red" className="size-5" />
               </span>
             </button>
-            <div className="flex flex-col md:flex-row gap-4 justify-between">
-              <section className="w-full md:w-2/3">
+
+
+            <div className="flex flex-col lg:flex-row gap-4 justify-between">
+              <section className="w-full lg:w-2/3">
                 {cartItems.map((item) => (
                   <div
                     key={item._id}
-                    className="bg-gray-50 p-4 rounded-lg shadow-md mb-4 relative transition-transform duration-300 hover:shadow-lg"
+                    className="bg-gray-50 py-4 px-4 rounded-lg shadow-md mb-4 relative transition-transform duration-300 hover:shadow-lg"
                   >
                     <div className="flex items-center gap-3">
                       <img
@@ -99,14 +102,14 @@ const Cart = () => {
                     </div>
                     <button
                       onClick={() => removeFromCart(item._id)}
-                      className="text-red-500 hover:text-red-700 transition duration-300 bg-red-100 rounded-full px-1 py-1 ml-4 absolute right-2 bottom-1/2 translate-y-1/2"
+                      className="text-red-500 hover:text-red-700 transition duration-300 bg-red-100 rounded-full px-1 py-1 ml-8 absolute right-4 bottom-8 md:bottom-1/2 translate-y-1/2"
                     >
                       <RiDeleteBin6Line size={24} />
                     </button>
                   </div>
                 ))}
               </section>
-              <section className="bg-gray-50 p-4 rounded-lg shadow-md mb-4 w-full md:w-1/3 h-fit">
+              <section className="bg-gray-50 px-6 py-10 rounded-lg shadow-md mb-4 w-full lg:w-1/3 h-fit">
                 <h2 className="text-2xl border-gray-400 border-b pb-2">
                   Checkout Summary
                 </h2>
