e30ed7117c163cac0ab547a812d7f4ababccb56c
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2025-01-06 13:44:48 +0600

    Updated margin issue

--- src/components/Ebook.jsx
+++ src/components/Ebook.jsx
@@ -7,7 +7,7 @@ const Ebook = () => {
   };
   return (
       <div
-        className="fixed left-5 top-3/4 -rotate-90 origin-left bg-gray-600 text-white px-3 py-2 rounded-b-md shadow-lg cursor-pointer text-sm md:text-base lg:text-lg"
+        className="fixed left-5 lg:top-1/2 top-3/4 -rotate-90 origin-left bg-primary text-white px-3 py-2 rounded-b-md shadow-lg cursor-pointer text-sm md:text-base lg:text-lg"
         // className="fixed left-5 top-1/2 transform -translate-y-1/2 -rotate-90 origin-left bg-gray-600 text-white px-3 py-2 rounded-b-md shadow-lg cursor-pointer text-sm md:text-base lg:text-lg"
         onClick={handleBook}
       >
